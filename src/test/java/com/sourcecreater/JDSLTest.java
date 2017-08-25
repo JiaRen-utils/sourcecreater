185341f8df68a1ae18c7c9ac34789ec7bd438c43
steve deng <529543738@qq.com>
2017-08-25 21:47:49 +0800

    add java data struct library

---
+++ src/test/java/com/sourcecreater/JDSLTest.java
@@ -0,0 +1,25 @@
+package com.sourcecreater;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import jdsl.core.api.Sequence;
+import jdsl.core.api.Tree;
+
+/**
+ * how to use jdsl(java data struct library) can view the websize below
+ * https://cs.brown.edu/cgc/jdsl/tutorial/lesson06/lesson06.html 
+ * @author steve
+ *
+ */
+public class JDSLTest {
+	  protected Tree tree; 
+	    protected Sequence names;
+	@Test
+	public void test() {
+	  
+
+	}
+
+}
